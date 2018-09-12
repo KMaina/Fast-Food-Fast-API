cfb5384e5777526e989e099265c15f749e923bfe
KMaina <maina.ken0@gmail.com>
2018-09-13 01:47:03 +0300

    Add a status key to the orders dictionary

--- app/views.py
+++ app/views.py
@@ -4,23 +4,26 @@ The views.py file.
 All routes in the app are located here
 """
 
-ORDERS = [
+orders = [
     {
         'name': 'Hamburger',
         'quantity' : 3,
         'description' : 'Tasty chicken burger perfect as a snack.',
-        'id' : 1
+        'id' : 1,
+        'status': 0
     },
     {
         'name': 'French Fries',
         'quantity' : 2,
         'description' : 'Perfectly salted for you.',
-        'id' : 2
+        'id' : 2,
+        'status': 0
     },
     {
         'name': 'Pizza',
         'quantity' : 5,
         'description' : 'Large pizza great for movie nights.',
-        'id' : 3
+        'id' : 3,
+        'status': 0
     }
 ]
