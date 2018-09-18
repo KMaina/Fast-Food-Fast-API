b441b461726040bdb0dd50c1a07594fce79febca
KMaina <maina.ken0@gmail.com>
2018-09-18 23:19:00 +0300

    [ch #160562279] Restructure the app folders

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 """
 the __init__.py file
 
-included to make app a module
+included to make app a package
 """
 
 from flask import Flask
 
+# from app import app
+
 app = Flask(__name__)
 
-from app import views
+from app.api.v1 import views
