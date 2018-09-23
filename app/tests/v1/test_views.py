7ad53f1bd556a068236a15d8345dc24d5e286af7
KMaina <maina.ken0@gmail.com>
2018-09-23 20:33:56 +0300

    [Chore #160602081] Refactor tests for the views

--- app/tests/v1/test_views.py
+++ app/tests/v1/test_views.py
@@ -4,28 +4,49 @@ Runs unit tests on the views.py routes
 
 """
 
-from app import app
+import unittest
+import json
+from app import create_app
 
-def test_one_order():
-    """Returns a 200 status code if an order is fetched"""
-    response = app.test_client().get('/api/v1/order/1')
-
-    assert response.status_code == 200
-
-def test_one_order_fails():
-    """Returns a 404 status code if an order is not fetched or found"""
-    response = app.test_client().get('/api/v1/order/')
-
-    assert response.status_code == 404
+class TestOrders(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app.config['TESTING'] = True
+        self.client = self.app.test_client
+        self.order = {
+	        "name": "Pizza",
+	        "quantity": 2,
+	        "description": "Perfect as a snack.",
+	        "status": 0
+        }   
+        self.changed_order = {
+	        "name": "Pizza",
+	        "quantity": 2,
+	        "description": "Perfect as a snack.",
+	        "status": 1
+        }   
     
-def test_all_orders():
-    """Returns a status code of 200
-    Tests if the route gets a list of all orders
-    """
-    response = app.test_client().get('/api/v1/orders')
-    assert response.status_code == 200
+    def test_add_an_order(self):
+        """Test for adding an order"""
+        response = self.client().post('/api/v1/orders', data=json.dumps(self.order), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+    
 
-def test_edit_order_fails():
-    """Returns a 404 status code if the order is not found"""
-    response = app.test_client().put('/api/v1/order/', data={'status': 1}, content_type='application/json')
-    assert response.status_code == 404
+    def test_get_all_orders(self):
+        """Test for fetching all orders"""
+        response = self.client().get('/api/v1/orders')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_specific_order(self):
+        """Test for fetching a specific order"""
+        response = self.client().get('/api/v1/order/1')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_change_an_order_change(self):
+        """Test for changing an order"""
+        response = self.client().post('/api/v1/orders', data=json.dumps(self.order), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        response = self.client().put('/api/v1/order/1', data=json.dumps(self.changed_order), content_type='application/json')
+        result = self.client().get('/api/v1/order/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('1', str(result.data))   
