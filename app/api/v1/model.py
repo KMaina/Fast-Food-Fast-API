e371ad078f2c4b745330255e2e56eed733b6c287
KMaina <maina.ken0@gmail.com>
2018-09-30 12:18:36 +0300

    [Feature #160806298] Check if inputs are of the right type

--- app/api/v1/model.py
+++ app/api/v1/model.py
@@ -11,4 +11,33 @@ class Users():
     """Class to handle users"""
     
     def register_user(self, username, password, confirm_password, email, address, telephone, admin):
-        pass
+        
+        if type(username) != str():
+            response = jsonify({'msg' : 'Username must be a string'})
+            response.status_code = 400
+            return response
+        
+        if type(password) != str() and type(confirm_password) != str():
+            response = jsonify({'msg' : 'Password must be a string'})
+            response.status_code = 400
+            return response
+        
+        if type(email) != str():
+            response = jsonify({'msg' : 'Email must be a string'})
+            response.status_code = 400
+            return response
+        
+        if type(address) != str():
+            response = jsonify({'msg' : 'Address must be a string'})
+            response.status_code = 400
+            return response
+        
+        if type(telephone) != str():
+            response = jsonify({'msg' : 'UsTelephoneername must be a string'})
+            response.status_code = 400
+            return response
+        
+        if type(admin) != bool():
+            response = jsonify({'msg' : 'Admin must be a boolean'})
+            response.status_code = 400
+            return response
