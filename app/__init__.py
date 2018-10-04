5b423a1e93db06b33ebb9cfa2f04fb866bc38910
KMaina <maina.ken0@gmail.com>
2018-10-04 09:58:58 +0300

    [Chore #160955995] Add model for database interactivity

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,10 @@ included to make app a package
 
 from flask import Flask
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
 
 from app.api.v2.views_users import UserRegister, UserLogin
+from app.api.v2.model_menu import Meals
 
 from instance.config import app_config
 
@@ -15,8 +17,12 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    
     api_endpoint = Api(app)
-    api_endpoint.add_resource(Orders, '/api/v1/orders')
-    api_endpoint.add_resource(OrderSpecific, '/api/v1/order/<int:order_id>')
+    api_endpoint.add_resource(UserRegister, '/api/v2/auth/signup')
+    api_endpoint.add_resource(UserLogin, '/api/v2/auth/login')
+    api_endpoint.add_resource(Meals, '/api/v2/menu')
+
+    jwt = JWTManager(app)
 
     return app
